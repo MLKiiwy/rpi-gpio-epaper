8430ec6ae452eec0f216288c7257e59d1983997e
Teis De Greve <teis@T.local>
2019-02-11 17:18:33 +0100

    Add two examples found on github gists

---
+++ examples/full-picture.js
@@ -0,0 +1,45 @@
+/**
+ * Example from https://gist.github.com/dbushell/dd36bef27ffd278140516151b1ed365c
+ */
+
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const EPD = require('rpi-gpio-epaper');
+
+function readImage(imagePath) {
+  return new Promise((resolve, reject) => {
+    fs
+      .createReadStream(imagePath)
+      .pipe(new PNG())
+      .on('parsed', function() {
+        const image = EPD.createImage(this.width, this.height);
+        for (let y = 0; y < this.height; y++) {
+          for (let x = 0; x < this.width; x++) {
+            image.view[y].writeUInt8(
+              EPD.toUInt8(
+                this.data[(this.width * y + x) << 2] > 128 ? 0xff : 0x00
+              ),
+              x,
+              1
+            );
+          }
+        }
+        resolve(image);
+      });
+  });
+}
+
+async function run() {
+  const epd = new EPD({
+    model: {
+      /* ...model constants */
+    }
+  });
+  await epd.init();
+  const image = await readImage(picPath);
+  const data = EPD.getImageRAM(image);
+  await epd.writeDisplayFull(data);
+  await epd.exit();
+}
+
+run();
---
+++ examples/picture.js
@@ -0,0 +1,48 @@
+/**
+ * Example from https://gist.github.com/dbushell/2207ee93f5d751c526c8909fd2f8ad28
+ */
+
+const path = require('path');
+const PNG = require('pngjs').PNG;
+const EPD = require('rpi-gpio-epaper');
+
+function readImage(imagePath) {
+  return new Promise((resolve, reject) => {
+    fs
+      .createReadStream(imagePath)
+      .pipe(new PNG())
+      .on('parsed', function() {
+        const image = EPD.createImage(this.width, this.height);
+        for (let y = 0; y < this.height; y++) {
+          for (let x = 0; x < this.width; x++) {
+            image.view[y].writeUInt8(
+              EPD.toUInt8(
+                this.data[(this.width * y + x) << 2] > 128 ? 0xff : 0x00
+              ),
+              x,
+              1
+            );
+          }
+        }
+        resolve(image);
+      });
+  });
+}
+
+async function run() {
+  let screen = new EPD();
+  await screen.init();
+
+  const imagePath = path.resolve(process.cwd(), 'picture.png');
+  const image = await readImage(imagePath);
+
+  const data = EPD.getImageRAM(image);
+  await screen.writeDisplayFull(data);
+
+  await screen.exit();
+  screen = null;
+
+  await EPD.sleep(500);
+}
+
+run();
