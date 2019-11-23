bc9ef5d2c4df55a9e9752e5b2f27b32bba380967
Mikael Labrut <labrut@gmail.com>
2019-11-23 19:33:46 +0100

    wip

--- examples/full-picture.js
+++ examples/full-picture.js
@@ -2,16 +2,15 @@
  * Incomplete example from https://gist.github.com/dbushell/dd36bef27ffd278140516151b1ed365c
  */
 
-const fs = require('fs');
-const PNG = require('pngjs').PNG;
-const EPD = require('rpi-gpio-epaper');
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const EPD = require("rpi-gpio-epaper");
 
 function readImage(imagePath) {
   return new Promise((resolve, reject) => {
-    fs
-      .createReadStream(imagePath)
+    fs.createReadStream(imagePath)
       .pipe(new PNG())
-      .on('parsed', function() {
+      .on("parsed", function() {
         const image = EPD.createImage(this.width, this.height);
         for (let y = 0; y < this.height; y++) {
           for (let x = 0; x < this.width; x++) {
@@ -30,13 +29,10 @@ function readImage(imagePath) {
 }
 
 async function run() {
-  const epd = new EPD({
-    model: {
-      /* ...model constants */
-    }
-  });
+  const epd = new EPD();
   await epd.init();
-  const image = await readImage(picPath);
+  const imagePath = path.resolve(process.cwd(), "test.png");
+  const image = await readImage(imagePath);
   const data = EPD.getImageRAM(image);
   await epd.writeDisplayFull(data);
   await epd.exit();
